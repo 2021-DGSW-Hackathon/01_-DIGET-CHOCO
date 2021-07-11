fd9bd478c52e0a47e260f305a30e9cb283a6cacb
krung2 <jungbin4337@dgsw.hs.kr>
2021-07-11 20:56:11 +0900

    Init: initialize

---
+++ webViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  webViewTest
+//
+//  Created by 신중빈 on 2021/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

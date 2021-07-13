c8576b172aaaa3012da7132536410d3bcd71e6cb
krung2 <jungbin4337@dgsw.hs.kr>
2021-07-13 19:55:21 +0900

    a

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Token } from 'src/libs/decorators/token.decorator';
+import AuthGaurd from 'src/middlewares/auth.middleware';
+import User from 'src/models/User';
+import BaseResponse from 'src/response/base.response';
 
 @Controller('user')
 export class UserController {
 
-  // @Post('')
+  @Get()
+  @UseGuards(AuthGaurd)
+  async getUser(
+    @Token() user: User,
+  ) {
+
+    return new BaseResponse(200, '조회 성공', user);
+  }
 }

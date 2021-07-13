1b93a12f605948cb0f7fab5fa2f2b9b8bb32f0be
krung2 <jungbin4337@dgsw.hs.kr>
2021-07-13 09:48:53 +0900

    Add: User Entity

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,39 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
 
 @Entity('user')
 export default class User {
 
-  // @
+  @PrimaryColumn({
+    name: 'user_id',
+    type: "varchar"
+  })
+  userId: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  grade: number;
+
+  @Column()
+  room: number;
+
+  @Column()
+  number: number;
+
+  @Column({
+    nullable: true,
+    name: 'profile_image',
+  })
+  profileImage?: string;
+
+  @CreateDateColumn({
+    name: 'created_at'
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at'
+  })
+  updatedAt: Date;
 }

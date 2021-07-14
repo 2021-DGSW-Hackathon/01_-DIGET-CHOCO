2ddc707740c1e767641438affd732c235bfadf7f
krung2 <jungbin4337@dgsw.hs.kr>
2021-07-14 11:48:38 +0900

    a

--- webViewTest/ViewController.swift
+++ webViewTest/ViewController.swift
@@ -12,51 +12,65 @@ import WebKit
 class ViewController:
     UIViewController,
     WKUIDelegate,
-    WKNavigationDelegate
+    WKNavigationDelegate,
+    UIScrollViewDelegate
 {
-
+    
     @IBOutlet weak var webView: WKWebView!
-        override func loadView() {
-                super.loadView()
-                
-                webView = WKWebView(frame: self.view.frame)
-                webView.uiDelegate = self
-                webView.navigationDelegate = self
-                self.view = self.webView
-            }
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-           
-            let url = URL(string: "http://matitsogo.kro.kr/")
-            let request = URLRequest(url: url!)
-//            self.webView?.allowsBackForwardNavigationGestures = true  //뒤로가기 제스쳐 허용
-            let preferences = WKPreferences()
-            preferences.javaScriptEnabled = true //자바스크립트 활성화
-            webView.load(request)
-
-        }
+    override func loadView() {
+        super.loadView()
         
-        override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() } //모달창 닫힐때 앱 종료현상 방지.
+        webView = WKWebView(frame: self.view.frame)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        self.view = self.webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        //alert 처리
-        func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String,
-                     initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void){
-            let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in completionHandler() }))
-            self.present(alertController, animated: true, completion: nil) }
-
-        //confirm 처리
-        func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
-            let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: { (action) in completionHandler(false) }))
-            alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in completionHandler(true) }))
-            self.present(alertController, animated: true, completion: nil) }
+        let url = URL(string: "http://matitsogo.kro.kr/")
+        let request = URLRequest(url: url!)
+        self.webView?.allowsBackForwardNavigationGestures = true  //뒤로가기 제스쳐 허용
+        self.webView.scrollView.delegate = self
+        webView.scrollView.isScrollEnabled = false
+        let preferences = WKPreferences()
+        preferences.javaScriptEnabled = true //자바스크립트 활성화
+        webView.load(request)
         
-        // href="_blank" 처리
-        func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
-            if navigationAction.targetFrame == nil { webView.load(navigationAction.request) }
-            return nil
-            
-        }
+    }
+    
+    override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() } //모달창 닫힐때 앱 종료현상 방지.
+    
+    //alert 처리
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String,
+                 initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void){
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in completionHandler() }))
+        self.present(alertController, animated: true, completion: nil) }
+    
+    //confirm 처리
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: { (action) in completionHandler(false) }))
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in completionHandler(true) }))
+        self.present(alertController, animated: true, completion: nil) }
+    
+    // href="_blank" 처리
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        if navigationAction.targetFrame == nil { webView.load(navigationAction.request) }
+        return nil
+        
+    }
+    
+    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+        scrollView.pinchGestureRecognizer?.isEnabled = false
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return nil
+    }
+    
+    
+    
 }
